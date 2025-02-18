74ccb8f4b00815c11a58892521e9662bef39a553
achraf <achrafsikal@gmail.com>
2025-02-18 09:57:30 +0100

    the full adder is now fully functional and excaptes other bits then 8-bit

--- script.py
+++ script.py
@@ -17,10 +17,11 @@ def fullAdder(a, b , c):
     else:
         return [int(sum[0]),int(Carry)]
 
+bits = int(input("how many bits it's in your number: "))
 def DecToBin(num):
-    binary_8bit = list(format(num, '08b'))  # '08b' means 8-bit binary
-    binary_8bit.reverse()
-    return binary_8bit
+    binary = list(format(num, '0'+str(bits)+'b'))  # '08b' means 8-bit binary
+    binary.reverse()
+    return binary
 
 def BinToDec(bin):
     return int(bin, 2)
@@ -35,32 +36,32 @@ res = []
 cache = [halfAdder(a[0],b[0])]
 print("cache 0:",cache)
 res.insert(0,cache[0][0])
-res.insert(0,fullAdder(a[1],b[1],cache[0][1])[0])
-cache.append(fullAdder(a[1],b[1],cache[0][1]))
-print("cache 1:",cache)
-res.insert(0,fullAdder(a[2],b[2],cache[1][1])[0])
-cache.append(fullAdder(a[2],b[2],cache[1][1]))
-print("cache 2:",cache)
-res.insert(0,fullAdder(a[3],b[3],cache[2][1])[0])
-cache.append(fullAdder(a[3],b[3],cache[2][1]))
-print("cache 3:",cache)
-res.insert(0,fullAdder(a[4],b[4],cache[3][1])[0])
-cache.append(fullAdder(a[4],b[4],cache[3][1]))
-print("cache 4:",cache)
-res.insert(0,fullAdder(a[5],b[5],cache[4][1])[0])
-cache.append(fullAdder(a[5],b[5],cache[4][1]))
-print("cache 5:",cache)
-res.insert(0,fullAdder(a[6],b[6],cache[5][1])[0])
-cache.append(fullAdder(a[6],b[6],cache[5][1]))
-res.insert(0,fullAdder(a[7],b[7],cache[6][1])[0])
-cache.append(fullAdder(a[7],b[7],cache[6][1]))
-# def bitadder(bit=7):
-#     for i in range(bit):
-#         # print("res.insert(0,fullAdder(a["+str(i+1)+"],b["+str(i+1)+"],cache["+str(i)+"][1])[0])\ncache.append(fullAdder(a["+str(i+1)+"],b["+str(i+1)+"],cache["+str(i)+"][1]))")
-#         res.insert(0,fullAdder(a[i+1],b[i+1],cache[i][1])[0])
-#         cache.append(fullAdder(a[i+1],b[i+1],cache[i][1]))
+# res.insert(0,fullAdder(a[1],b[1],cache[0][1])[0])
+# cache.append(fullAdder(a[1],b[1],cache[0][1]))
+# print("cache 1:",cache)
+# res.insert(0,fullAdder(a[2],b[2],cache[1][1])[0])
+# cache.append(fullAdder(a[2],b[2],cache[1][1]))
+# print("cache 2:",cache)
+# res.insert(0,fullAdder(a[3],b[3],cache[2][1])[0])
+# cache.append(fullAdder(a[3],b[3],cache[2][1]))
+# print("cache 3:",cache)
+# res.insert(0,fullAdder(a[4],b[4],cache[3][1])[0])
+# cache.append(fullAdder(a[4],b[4],cache[3][1]))
+# print("cache 4:",cache)
+# res.insert(0,fullAdder(a[5],b[5],cache[4][1])[0])
+# cache.append(fullAdder(a[5],b[5],cache[4][1]))
+# print("cache 5:",cache)
+# res.insert(0,fullAdder(a[6],b[6],cache[5][1])[0])
+# cache.append(fullAdder(a[6],b[6],cache[5][1]))
+# res.insert(0,fullAdder(a[7],b[7],cache[6][1])[0])
+# cache.append(fullAdder(a[7],b[7],cache[6][1]))
+def bitadder(bit):
+    for i in range(bit):
+        # print("res.insert(0,fullAdder(a["+str(i+1)+"],b["+str(i+1)+"],cache["+str(i)+"][1])[0])\ncache.append(fullAdder(a["+str(i+1)+"],b["+str(i+1)+"],cache["+str(i)+"][1]))")
+        res.insert(0,fullAdder(a[i+1],b[i+1],cache[i][1])[0])
+        cache.append(fullAdder(a[i+1],b[i+1],cache[i][1]))
 
-# bitadder()
+bitadder(bits-1)
 
 
 res = "".join(map(str,res))
